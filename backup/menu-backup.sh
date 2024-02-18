8c1d6fef8d17689a6efe9b26fffb5bfe08fc4194
gazzent <158091173+gazzent@users.noreply.github.com>
2024-02-19 05:31:13 +0700

    Update and rename menu-backup (1).sh to menu-backup.sh

--- backup/menu-backup.sh
+++ backup/menu-backup.sh
@@ -18,7 +18,7 @@ echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
 echo -e
 echo -e "${BlueCyan}              Menu Backup                   "
 echo -e
-echo -e "            t.me/todfix667"
+echo -e "            t.me/Candravpnz"
 echo -e "${ungu}++++++++++++++++++++++++++++++++++++++++++++"
 echo
 echo
@@ -41,7 +41,7 @@ figlet "backup" | lolcat
 backup
 echo "--------------------------------------------------------"
 echo
-echo "Terimakasih sudah menggunakan layanan script todfix667"
+echo "Terimakasih sudah menggunakan layanan script candra"
 echo
 echo "--------------------------------------------------------"
 fi
@@ -52,7 +52,7 @@ figlet "Restore" | lolcat
 restore
 echo "--------------------------------------------------------"
 echo
-echo "Terimakasih sudah menggunakan layanan script todfix667"
+echo "Terimakasih sudah menggunakan layanan script Candra"
 echo
 echo "--------------------------------------------------------"
 fi
@@ -62,7 +62,7 @@ figlet "strt"
 strt
 echo "--------------------------------------------------------"
 echo
-echo "Terimakasih sudah menggunakan layanan script todfix667"
+echo "Terimakasih sudah menggunakan layanan script candra"
 echo
 echo "--------------------------------------------------------"
 fi
@@ -71,7 +71,7 @@ then
 limitspeed
 echo "--------------------------------------------------------"
 echo
-echo "Terimakasih sudah menggunakan layanan script todfix667"
+echo "Terimakasih sudah menggunakan layanan script candra"
 echo
 echo "--------------------------------------------------------"
 fi
@@ -81,7 +81,7 @@ then
 autobackup
 echo "--------------------------------------------------------"
 echo
-echo "Terimakasih sudah menggunakan layanan script todfix667"
+echo "Terimakasih sudah menggunakan layanan script candra"
 echo
 echo "--------------------------------------------------------"
 fi
