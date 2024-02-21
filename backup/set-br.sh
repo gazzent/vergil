d9b0acd017c8d455101c41814e4fbfb1f92f2900
gazzent <158091173+gazzent@users.noreply.github.com>
2024-02-21 08:11:56 +0700

    Update set-br.sh

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -14,7 +14,7 @@ LIGHT='\033[0;37m'
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/NevermoreSSH/Vergil/main/backup"
+akbarvpn="raw.githubusercontent.com/gazzent/vergil/main/backup"
 
 apt install rclone -y
 printf "q\n" | rclone config
@@ -43,11 +43,11 @@ logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/backup/autobackup.sh"
-wget -O backup "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/backup/backup.sh"
-wget -O restore "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/backup/restore.sh"
-wget -O strt "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/backup/strt.sh"
-wget -O limitspeed "https://raw.githubusercontent.com/NevermoreSSH/Vergil/main/backup/limitspeed.sh"
+wget -O autobackup "https://raw.githubusercontent.com/gazzent/vergil/main/backup/autobackup.sh"
+wget -O backup "https://raw.githubusercontent.com/gazzent/vergil/main/backup/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/gazzent/vergil/main/backup/restore.sh"
+wget -O strt "https://raw.githubusercontent.com/gazzent/vergil/main/backup/strt.sh"
+wget -O limitspeed "https://raw.githubusercontent.com/gazzent/vergil/main/backup/limitspeed.sh"
 chmod +x autobackup
 chmod +x backup
 chmod +x restore
